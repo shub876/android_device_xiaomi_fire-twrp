0bfb837af4e9ddda5882365217113acf67f30510
shub876 <8706shubham@gmail.com>
2024-03-21 00:02:00 +0530

    Tree: Switch to OFRP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,5 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_fire.mk
+    $(LOCAL_DIR)/ofrp_fire.mk
 
-COMMON_LUNCH_CHOICES := twrp_fire-eng
+COMMON_LUNCH_CHOICES := ofrp_fire-eng
     
